dc9339a8e516626a31b09e771b064cec5813eedd
iwumezie Pamela <iwumeziep@gmail.com>
2023-02-01 05:15:25 +0100

    add forward slashes and closing brackets to my page files

--- src/App.js
+++ src/App.js
@@ -21,11 +21,11 @@ function App() {
       <Sidebar /> {/*for small screen*/}
       <Routes>
         <Route path="/" element={<HomePage/>} />
-        <Route path="about" element={<AboutPage/>} />
-        <Route path="cart" element={<CartPage/>} />
-        <Route path="checkout" element={<CheckoutPage/>} />
-        <Route path="private" element={<PrivateRoute/>} />
-        <Route path="product" element={<ProductsPage/>} />
+        <Route path="/about" element={<AboutPage/>} />
+        <Route path="/cart" element={<CartPage/>} />
+        <Route path="/checkout" element={<CheckoutPage/>} />
+        <Route path="/private" element={<PrivateRoute/>} />
+        <Route path="/product" element={<ProductsPage/>} />
         <Route path="/products/:id" element={<SingleProductPage/>} />
         <Route path="*" element={<ErrorPage/>} />
       </Routes>
